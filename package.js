22dcaefc4e59cc4289267f5582f8e86319fb8f5e
alethes <alethesdev@icloud.com>
2014-08-29 11:00:49 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,5 +1,74 @@
-{
+Package.describe({
   "summary": "State of the art, out of the box Meteor pagination",
   "version": "v1.0.23",
   "git": "https://github.com/alethes/meteor-pages"
-}
+});
+
+Package.on_use(function(api){
+    api.versionsFrom("METEOR@0.9.0")
+    api.use([
+        "deps",
+        "underscore",
+        "coffeescript"
+    ]);
+
+    api.use([
+        "templating",
+        "handlebars",
+        "ui",
+        "session"
+    ], "client");
+
+    api.use([
+        "deps",
+        "underscore",
+        "coffeescript",
+        "check"
+    ], "server");
+
+    api.add_files([
+        "lib/pages.coffee"
+    ]);
+
+    api.add_files([
+        "client/templates.html",
+        "client/controllers.coffee",
+        "client/main.css",
+        "public/loader.gif"
+    ], "client");
+});
+
+Package.on_test(function(api){
+    api.versionsFrom("METEOR@0.9.0")
+    api.use([
+        "deps",
+        "underscore",
+        "coffeescript"
+    ]);
+
+    api.use([
+        "templating",
+        "handlebars",
+        "ui",
+        "session"
+    ], "client");
+
+    api.use([
+        "deps",
+        "underscore",
+        "coffeescript",
+        "check"
+    ], "server");
+
+    api.add_files([
+        "lib/pages.coffee",
+        "test/tests.coffee"
+    ]);
+
+    api.add_files([
+        "client/templates.html",
+        "client/controllers.coffee",
+        "client/main.css",
+        "public/loader.gif"
+    ], "client");
+});
