1f7ac3c1036d8518333b7c66358d46a37b6fa158
alethes <alethesdev@icloud.com>
2014-09-26 01:33:14 +0200

    Add package name

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
+  "name": "alethes:pages"
   "summary": "State of the art, out of the box Meteor pagination",
-  "version": "1.1.0",
+  "version": "1.1.1",
   "git": "https://github.com/alethes/meteor-pages"
 });
 
