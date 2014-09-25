68e71bcb367a5a8c1aa44ed617e2af2a974e3289
alethes <alethesdev@icloud.com>
2014-09-26 01:37:22 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   "name": "alethes:pages",
   "summary": "State of the art, out of the box Meteor pagination",
-  "version": "1.1.1",
+  "version": "1.1.2",
   "git": "https://github.com/alethes/meteor-pages"
 });
 
@@ -28,45 +28,7 @@ Package.on_use(function(api){
     ], "server");
 
     api.add_files([
-        "lib/pages.coffee"
-    ]);
-
-    api.add_files([
-        "client/templates.html",
-        "client/controllers.coffee",
-        "client/main.css",
-        "public/loader.gif"
-    ], "client");
-});
-
-Package.on_test(function(api){
-    api.versionsFrom("METEOR@0.9.0")
-    api.use([
-        "deps",
-        "underscore",
-        "coffeescript"
-    ]);
-
-    api.use([
-        "templating",
-        "handlebars",
-        "ui",
-        "session"
-    ], "client");
-
-    api.use([
-        "deps",
-        "underscore",
-        "coffeescript",
-        "check"
-    ], "server");
-
-    api.add_files([
         "lib/pages.coffee",
-        "test/tests.coffee"
-    ]);
-
-    api.add_files([
         "client/templates.html",
         "client/controllers.coffee",
         "client/main.css",
