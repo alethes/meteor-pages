ac08f252527a3aa7c8078933e68e02a26522372f
alethes <alethesdev@icloud.com>
2014-10-27 12:11:49 +0100

    Fix basic example

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   "name": "alethes:pages",
   "summary": "State of the art, out of the box Meteor pagination",
-  "version": "1.3.0",
+  "version": "1.3.1",
   "git": "https://github.com/alethes/meteor-pages"
 });
 
