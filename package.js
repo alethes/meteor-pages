ba03a975d3827bfbd1cdbf36127e0790caed02ba
alethes <alethesdev@icloud.com>
2015-02-13 03:24:43 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   "name": "alethes:pages",
   "summary": "State of the art, out of the box Meteor pagination",
-  "version": "1.7.2",
+  "version": "1.8.0",
   "git": "https://github.com/alethes/meteor-pages"
 });
 
